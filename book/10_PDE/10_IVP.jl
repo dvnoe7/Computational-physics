b5ab1c0a93ea5087be7d7038fc9d62bef0a6c2e3
dvnoe7 <dvnoeedu@gmail.com>
2025-11-26 19:46:18 +0100

    Create 10_IVP.jl

---
+++ book/10_PDE/10_IVP.jl
@@ -0,0 +1,32 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ dba5e2a0-caf7-11f0-af49-21f2171b37ec
+begin
+	Tleft = 50
+	Tright = 0
+	Tmidle =20
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.1"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═dba5e2a0-caf7-11f0-af49-21f2171b37ec
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
